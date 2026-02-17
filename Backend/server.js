501919e63626cac9faa49bbca6adbe8ddf9ef2ad
Sohail <sohailsaifi09876@gmail.com>
2026-02-17 22:23:10 +0530

    Updated Port configuration for Render

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
 const app = require('./src/app')
 const connectdb = require('./src/DB/db')
+const PORT = process.env.PORT || 3000;
 
 
 connectdb()
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log("server is runing on port 3000")
 });
 
